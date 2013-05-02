d4b806f331a685db85adfcf7ff032d7d6bc23228
Toni Tassani <atassani@gmail.com>
2013-05-02 18:08:53 +0200

    Initial.

---
+++ svn_ignore.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+for f in `find . -name .svn`; do svn delete --force $f; done
+for f in `find . -name .classpath`; do svn delete --force $f; done
+for f in `find . -name .preferences`; do svn delete --force $f; done
+for f in `find . -name .settings`; do svn delete --force $f; done
+for f in `find . -name .project`; do svn delete --force $f; done
+for f in `find . -name target`; do svn delete --force $f; done
+for f in `find . -name bin`; do svn delete --force $f; done
+
+for d in *; do
+ svn propset svn:ignore -F "c:\workarea\code\ignore_list.txt" $d
+done
