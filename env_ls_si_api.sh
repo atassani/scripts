050abbdef18836847b79744cc33a7f5a75148757
Toni Tassani <toni.tassani@thomsonreuters.com>
2014-09-08 12:13:42 +0200

    Enviornment for LS-SI-API

---
+++ env_ls_si_api.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash -x
+
+#export MR_HOME="E:\workarea\code\LS-SI-API\app-conf\dev-dtc"
+export MR_HOME=/cygdrive/e/workarea/code/LS-SI-API/app-conf/dev-dtc
+export NG_ENV=dev_dtc
+export JBOSS_CONF=defaultapilayer
+export PROJECT_DIR=/cygdrive/e/workarea/code/LS-SI-API/ls-si-api-mr-war/target
+export PROJECT_FILE=ls-si-api-mr-war-*.war
